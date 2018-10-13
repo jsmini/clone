db35b438ca0cf27abb09cf4c4d68ac055fb57de6
颜海镜 <yanhaijing@yeah.net>
2018-10-13 12:28:35 +0800

    refactor: 优化性能测试代码

---
+++ demo/js/comm.js
@@ -0,0 +1,55 @@
+var clone = jsmini_clone.clone;
+var cloneJSON = jsmini_clone.cloneJSON;
+var cloneLoop = jsmini_clone.cloneLoop;
+var cloneForce = jsmini_clone.cloneForce;
+
+function createData(deep, breadth) {
+    var data = {};
+    var temp = data;
+
+    for (var i = 0; i < deep; i++) {
+        temp = temp['data'] = {};
+        for (var j = 0; j < breadth; j++) {
+            temp[j] = j;
+        }
+    }
+
+    return data;
+}
+
+function runCount(fn, count) {
+    var time = Date.now();
+    while(count--) {
+        fn()
+    }
+
+    return Date.now() - time;
+}
+
+function runTime(fn, time) {
+    var stime = Date.now();
+    var count = 0;
+    while(Date.now() - stime < time) {
+        fn();
+        count++;
+    }
+
+    return count;
+}
+
+function safeRun(name, fn) {
+    console.log(name, ' start');
+    
+    var time = Date.now();
+
+    try {
+        fn()
+    } catch(e) {
+        console.log(e.message)
+    }
+
+    time = Date.now() - time;
+
+    console.log(name, ' end，耗时: ', time);
+    return time;
+}
